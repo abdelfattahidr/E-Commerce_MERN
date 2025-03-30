2f0c24b30cc1cb496626c9627ac723c72e745806
abdelfattah idr <76259345+abdelfattahidr@users.noreply.github.com>
2025-03-30 18:03:48 +0000

    clrear cart / put

--- Server/src/routers/cart.router.ts
+++ Server/src/routers/cart.router.ts
@@ -1,31 +1,51 @@
 import express from "express";
 import {
   addProductToCart,
+  deleteProductFromCart,
   getActiveCartForUser,
   updateProductToCart,
+  clearCart,
 } from "../servers/cartServes";
 import validateJWT from "../middlewares/validateJWT";
 import { ExtendRequest } from "../types/express";
 const router = express.Router();
 
-router
-  .get("/", validateJWT, async (req: ExtendRequest, res) => {
-    const userId = req?.user?._id;
+router.get("/", validateJWT, async (req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
 
-    const cart = await getActiveCartForUser({ userId });
-    res.status(200).json(cart);
-  })
-  .post("/", validateJWT, async (req: ExtendRequest, res) => {
+  const cart = await getActiveCartForUser({ userId });
+  res.status(200).json(cart);
+});
+
+router.post("/", validateJWT, async (req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
+  const { productId, quantity } = req.body;
+  const response = await addProductToCart({ userId, productId, quantity });
+  res.status(response.statusCode).json(response.data);
+});
+
+router.put("/", validateJWT, async (req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
+  const { productId, quantity } = req.body;
+  const response = await updateProductToCart({ userId, productId, quantity });
+  res.status(response.statusCode).json(response.data);
+});
+
+router.delete(
+  "/items/:productId",
+  validateJWT,
+  async (req: ExtendRequest, res) => {
     const userId = req?.user?._id;
-    const { productId, quantity } = req.body;
-    const response = await addProductToCart({ userId, productId, quantity });
+    const { productId } = req.params;
+    const response = await deleteProductFromCart({ userId, productId });
     res.status(response.statusCode).json(response.data);
-  })
-  .put("/", validateJWT, async (req: ExtendRequest, res) => {
-    const userId = req?.user?._id;
-    const { productId, quantity } = req.body;
-    const response = await updateProductToCart({ userId, productId, quantity });
-    res.status(response.statusCode).json(response.data);
-  });
+  }
+);
+
+router.delete("/", validateJWT, async (req: ExtendRequest, res) => {
+  const userId = req?.user?._id;
+  const response = await clearCart({ userId });
+  res.status(response.statusCode).json(response.data);
+});
 
 export { router as cartRouter };
--- Server/src/servers/cartServes.ts
+++ Server/src/servers/cartServes.ts
@@ -1,4 +1,4 @@
-import { cartModel } from "../models/cart.model";
+import { cartModel, ICartItem } from "../models/cart.model";
 import { productModel } from "../models/product.model";
 
 interface CreateCartForUser {
@@ -22,6 +22,18 @@ export const getActiveCartForUser = async ({
   }
   return cart;
 };
+
+interface ClearCart {
+  userId: string;
+}
+export const clearCart = async ({ userId }: ClearCart) => {
+  const cart = await getActiveCartForUser({ userId });
+  cart.items = [];
+  cart.totalAmount = 0;
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
+};
+
 interface AddProductToCart {
   userId: string;
   productId: any;
@@ -97,9 +109,7 @@ export const updateProductToCart = async ({
     (p) => p.product.toString() !== productId
   );
 
-  let total = otherCartItems.reduce((acc, pro) => {
-    return (acc += pro.quantity * pro.unitPrice);
-  }, 0);
+  let total = calculateTotalAmount({ cartItems: otherCartItems });
 
   existInCart.quantity = quantity;
   total += existInCart.quantity * existInCart.unitPrice;
@@ -107,4 +117,43 @@ export const updateProductToCart = async ({
 
   const updatedCart = await cart.save();
   return { data: updatedCart, statusCode: 200 };
+};
+
+interface DeleteProductFromCart {
+  userId: string;
+  productId: any;
+}
+
+export const deleteProductFromCart = async ({
+  userId,
+  productId,
+}: DeleteProductFromCart) => {
+  const cart = await getActiveCartForUser({ userId });
+
+  const existInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+  if (!existInCart) {
+    return { data: "Product dose not exists in cart", statusCode: 400 };
+  }
+
+  const otherCartItems = cart.items.filter(
+    (p) => p.product.toString() !== productId
+  );
+
+  let total = calculateTotalAmount({ cartItems: otherCartItems });
+
+  cart.items = otherCartItems;
+  cart.totalAmount = total;
+
+  const updatedCart = await cart.save();
+  return { data: updatedCart, statusCode: 200 };
+};
+
+const calculateTotalAmount = ({ cartItems }: { cartItems: ICartItem[] }) => {
+  const total = cartItems.reduce((acc, pro) => {
+    return (acc += pro.quantity * pro.unitPrice);
+  }, 0);
+
+  return total;
 };
